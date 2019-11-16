36881b25dabdb7fea1f97233a8db98edc2959bfc
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-11-16 14:58:43 +0900

    :bug: Fix errors returned by devtools::check()

--- R/convert_decimals.R
+++ R/convert_decimals.R
@@ -4,6 +4,8 @@
 #'
 #' @param x The first character
 #' @param y The other
+#' @param ... Additional arguments to be concatenated
+#' @param sep Separate character between two args
 #' @param concat If FALSE, x will be returned
 toggle_paste <- function(x, y, ..., sep = "", concat = TRUE) {
   if (concat == FALSE) return(as.character(x))
@@ -13,6 +15,7 @@ toggle_paste <- function(x, y, ..., sep = "", concat = TRUE) {
 #' Convert decimals
 #'
 #' @param value Value to be converted
+#' @param ... Arguments to be passed to methods
 #' @export
 conv <- function(value, ...) {
   UseMethod("conv")
@@ -22,7 +25,7 @@ conv <- function(value, ...) {
 #'
 #' @param catch_kg Catch in kilogram
 #' @param to decimal order to convert in character such as
-#' \description{
+#' \describe{
 #'   \item{"ton"}{ton}
 #'   \item{"dton"}{10 ton}
 #'   \item{"cton"}{100 ton}
@@ -34,13 +37,13 @@ conv.catch_kg <- function(catch_kg, to, add_jpdigit = TRUE) {
   switch(to,
          "ton"    = toggle_paste(round(catch_kg / 10 ^ 3, 1),  "",
                                   concat = add_jpdigit),
-         "dton"   = toggle_paste(round(catch_kg / 10 ^ 4, 1), "十",
+         "dton"   = toggle_paste(round(catch_kg / 10 ^ 4, 1), "\u5341",
                                   concat = add_jpdigit),
-         "cton"   = toggle_paste(round(catch_kg / 10 ^ 5, 1), "百",
+         "cton"   = toggle_paste(round(catch_kg / 10 ^ 5, 1), "\u767e",
                                   concat = add_jpdigit),
-         "kton"   = toggle_paste(round(catch_kg / 10 ^ 6, 1), "千",
+         "kton"   = toggle_paste(round(catch_kg / 10 ^ 6, 1), "\u5343",
                                   concat = add_jpdigit),
-         "10kton" = toggle_paste(round(catch_kg / 10 ^ 7, 1), "万",
+         "10kton" = toggle_paste(round(catch_kg / 10 ^ 7, 1), "\u4e07",
                                   concat = add_jpdigit))
 }
 
@@ -49,14 +52,14 @@ conv.catch_kg <- function(catch_kg, to, add_jpdigit = TRUE) {
 #' @inheritParams conv.catch_kg
 #' @param r_106fish Number of recruitments in "million fish"
 #' @param to decimal order to convert in character such as
-#' \description{
+#' \describe{
 #'   \item{"mil"}{million fishes}
 #'   \item{"100mil"}{100 million fishes, "oku" in Japanese}
 #'   #' }
 conv.recruit_106fish <- function(r_106fish, to, add_jpdigit = TRUE) {
   switch(to,
-         "mil"    = toggle_paste(round(r_106fish,       1), "百万",
+         "mil"    = toggle_paste(round(r_106fish,       1), "\u767e\u4e07",
                                   concat = add_jpdigit),
-         "100mil" = toggle_paste(round(r_106fish / 100, 1), "億",
+         "100mil" = toggle_paste(round(r_106fish / 100, 1), "\u5104",
                                   concat = add_jpdigit))
 }
